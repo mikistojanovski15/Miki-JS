0c54dad1a38ef49229880b55ab457e39ad76b0f3
Miki <145859082+mikistojanovski15@users.noreply.github.com>
2023-11-11 22:02:22 +0100

    Add files via upload
    JS domasna 1

---
+++ hm1.js
@@ -0,0 +1,77 @@
+// let hasCoffee = true;
+// let hasTea = false;
+// let hasMilk = true;
+
+// // a.
+// let student = !hasTea || hasMilk;
+// console.log(student);
+
+// // b.
+
+// let student1 = hasCoffee && !hasTea;
+// console.log(student1);
+
+// // c.
+
+// let student2 = !hasTea;
+// console.log(student2);
+
+// // d.
+
+// let student3 = (hasCoffee && hasMilk) != (!hasCoffee && !hasMilk);
+// console.log(student3);
+
+// // e.
+
+// let student4 = (!hasTea && hasMilk) || (hasCoffee && !hasTea);
+// console.log(student4);
+
+// // f.
+
+// let student5 = !hasCoffee || hasTea;
+// console.log(student5);
+
+// 2.
+
+// const num1 = 10;
+// const num2 = 5;
+// const num3 = 8;
+// const num4 = 2;
+// const num5 = 12;
+// const num6 = 4;
+// const num7 = 3;
+
+// let result1 = (num1 + num2) / (num5 + num6)
+// console.log(result1)
+
+// let result2 = (num5 - num2) + (num1 + num7)
+// console.log(result2)
+
+// let result3 = (num7 + num6) * (num5 + num2)
+// console.log(result3)
+
+// let result4 = (num4 * num2) / (num5 + num6)
+// console.log(result4)
+
+// let result5 = (num7 + num2) - (num3 + num4)
+// console.log(result5)
+
+// 3.
+
+// let num1 = 12
+// let num2 = 4
+
+// let sum = (num1 + num2)
+// console.log(sum)
+
+// let diff = (num1 - num2)
+// console.log(diff)
+
+// let mult = (num1 * num2)
+// console.log(mult)
+
+// let div = (num1 / num2)
+// console.log(div)
+
+// let rem = (num1 % num2)
+// console.log(rem)
